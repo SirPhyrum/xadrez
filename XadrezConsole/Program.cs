e9902ab06a5b92879aa5e2b5bc734998fffacfda
Thiago Fernandes <thiagofernandes442@hotmail.com>
2024-07-01 17:51:39 -0300

    Tratamento de erros de peças sobrepostas e peças em posição fora da matriz

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Tabuleiro;
 using Xadrez;
+using XadrezConsole.Tabuleiro;
 
 namespace XadrezConsole
 {
@@ -8,14 +9,21 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8,8);
-            
+            try
+            {
+                Board board = new Board(8, 8);
 
 
-            Piece p = new Torre(Color.White, board);
-            board.SetPiece(p, new Position(0,0));
 
-            Screen.PrintBoard(board);
+                Piece p = new Torre(Color.White, board);
+                board.SetPiece(p, new Position(0, 0));
+                board.SetPiece(p, new Position(0, 10));
+                Screen.PrintBoard(board);
+            }
+            catch (BoardException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- XadrezConsole/tabuleiro/Board.cs
+++ XadrezConsole/tabuleiro/Board.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XadrezConsole.Tabuleiro;
 
 namespace Tabuleiro
 {
-     class Board
+    class Board
     {
         public int Lines { get; set; }
         public int Columns { get; set; }
@@ -17,13 +18,43 @@ namespace Tabuleiro
         {
             Lines = lines;
             Columns = columns;
-            Pieces = new Piece[Lines,Columns];
+            Pieces = new Piece[Lines, Columns];
+        }
+
+        public Piece Piece(Position position)
+        {
+
+            return Pieces[position.Line, position.Column];
+        }
+
+        public bool ExistPiece(Position pos)
+        {
+            ValidatePosition(pos);
+            return Piece(pos) != null;
         }
 
         public void SetPiece(Piece p, Position pos)
         {
-            Pieces[pos.Line,pos.Column] = p;
+            if(ExistPiece(pos)) 
+                throw new BoardException("Já existe uma peça na posição escolhida!");
+
+            Pieces[pos.Line, pos.Column] = p;
             p.Position = pos;
         }
+
+        public bool ValidPosition(Position pos)
+        {
+            if(pos.Line < 0 || pos.Line >= Lines || pos.Column < 0 || pos.Column >= Columns)
+                return false;
+            return true;
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!ValidPosition(pos))
+                throw new BoardException("Posição inválida!");
+        }
+
     }
 }
+
